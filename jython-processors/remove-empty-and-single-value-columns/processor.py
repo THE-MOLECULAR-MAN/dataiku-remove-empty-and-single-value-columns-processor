ba312b7791ca6d51ba2e44cd5a3538f43c202176
admin <admin>
2025-01-11 14:21:14 -0500

    Edited file 'jython-processors/remove-empty-and-single-value-columns/processor.py'

--- jython-processors/remove-empty-and-single-value-columns/processor.py
+++ jython-processors/remove-empty-and-single-value-columns/processor.py
@@ -7,5 +7,23 @@
 # Define here a function that returns the result of the step.
 def process(rows):
     # row is a dict of the row on which the step is applied
+    
 
+    def find_empty_columns(df):
+        """x"""
+        return [col for col in df.columns if df[col].isnull().all()]
+    
+    df = pd.DataFrame(rows)
+
+
+    python_test1_df = apple_screentime_csv_tables_bespin_2024_03_08T18___47___06_05___00_ZOBJECT_df.drop(find_empty_columns(apple_screentime_csv_tables_bespin_2024_03_08T18___47___06_05___00_ZOBJECT_df),
+            axis=1)
+    # python_test1_df.dropna(thresh=2, axis=1, inplace=True)
+
+    for col in python_test1_df:
+        if(len(python_test1_df.loc[:,col].unique()) < 2):
+            python_test1_df.pop(col)
+
+    
+    
     return str(type(rows))
