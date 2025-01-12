64420afb58a84334ed29674562d22e72b6343557
admin <admin>
2025-01-12 04:57:36 -0500

    Edited file 'jython-processors/remove-empty-and-single-value-columns/processor.py'

--- jython-processors/remove-empty-and-single-value-columns/processor.py
+++ jython-processors/remove-empty-and-single-value-columns/processor.py
@@ -6,6 +6,10 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
                     format='remove-empty-and-single-value-columns-processor plugin %(levelname)s - %(message)s')
 
+remove_empty_columns = params.get('remove_empty_columns')
+remove_columns_with_only_one_value = params.get('remove_columns_with_only_one_value')
+
+
 # global- and project-level variables are passed as a dss_variables dict
 
 # the step parameters are passed as a params dict
